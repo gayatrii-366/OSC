7b526ef164f5c7772b912aa4d04c75ee11834c49
gayatrii <gayatri.swami@mitwpu.edu.in>
2026-08-13 13:59:46 +0530

    Refactor pipe.c for improved readability

--- pipe.c
+++ pipe.c
@@ -3,25 +3,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/ipc.h>
+
 #define MAX 20
+
 int main(){
-int filedes[2],n;
-char string[MAX];
-char line[MAX];
-pid_t pid;
-if((pipe(filedes))<0){
-	printf("\n pipe creation error");
+	int filedes[2],n;
+	char string[MAX];
+	char line[MAX];
+	
+	pid_t pid;
+	if((pipe(filedes))<0){
+		printf("\n pipe creation error");
 	exit(0);
 }
-if(pid>0){
-close(filedes[0]);
+	if(pid>0){
+		close(filedes[0]);
 write(filedes[1],string,MAX);
 }
-if(pid==0){
-close(filedes[1]);
-n=read(filedes[0],line,MAX);
-line[n]='\0';
-printf("\n\n Data read by child is:%s",line);
+	if(pid==0){
+		close(filedes[1]);
+		n=read(filedes[0],line,MAX);
+		line[n]='\0';
+		printf("\n\n Data read by child is:%s",line);
 
 }
 
