6aca8a7530ff8a22482de902d1f047057bb38778
gayatrii <gayatri.swami@mitwpu.edu.in>
2026-08-13 14:51:22 +0530

    Add files via upload

--- pipe.c
+++ pipe.c
@@ -1,42 +1,31 @@
-#include<sys/types.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/ipc.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
 
-#define MAX 20
+int main() {
+    int fd[2]; 
+    char read_msg[100]; 
+    char input_msg[100]; 
+    printf("Enter a message for the child: ");
+    scanf("%99s", input_msg);
 
-int main(){
-	int filedes[2],n;
-	char string[MAX];
-	char line[MAX];
-	
-	pid_t pid;
-	if((pipe(filedes))<0){
-		printf("\n pipe creation error");
-	exit(0);
-}
-	if(pid>0){
-		close(filedes[0]);
-write(filedes[1],string,MAX);
-}
-	if(pid==0){
-		close(filedes[1]);
-		n=read(filedes[0],line,MAX);
-		line[n]='\0';
-		printf("\n\n Data read by child is:%s",line);
+    pipe(fd); 
 
-}
-
-exit(0);
-
-
+    if (fork() != 0) { 
+        close(fd[0]); 
+        write(fd[1], input_msg, sizeof(input_msg)); 
+        close(fd[1]); 
+        wait(NULL);    
+    } else { 
+        close(fd[1]); 
+        read(fd[0], read_msg, sizeof(read_msg)); 
+        printf("Child received: %s\n", read_msg); 
+        close(fd[0]); 
+    }
+    
+    return 0;
 }
 
-/*computer@computerVY:~$ gcc pipe.c
-computer@computerVY:~$ ./a.out
 
 
- Data read by child is:computer@computerVY:~$ ^C
-computer@computerVY:~$ gedit pipe.c
-*/
